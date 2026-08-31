23015c0d55d1baba28c2db8e013e8bdb7f11b4c6
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2026-08-31 22:17:01 +0530

    Update Java_Stdin_and_Stdout_I.java

--- Java_Stdin_and_Stdout_I.java
+++ Java_Stdin_and_Stdout_I.java
@@ -10,7 +10,6 @@ public class  Java_Stdin_and_Stdout_I{
         scan.close();
         System.out.println(a);
         System.out.println(b);
-        System.out.println(c);
-        
+        System.out.println(c);   
     }
 }
